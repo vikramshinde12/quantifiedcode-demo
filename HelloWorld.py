4942e297fe22bcf41f6f7493760fd876d3480ac8
Vikram P. Shinde <vikramps@mastek.com>
2016-07-13 10:00:19 +0100

    Added on 13/07/2016

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,13 +4,5 @@ def helloWorld(myString):
     print("Hello : "+myString)
     my_name = input("What is your name?")
     print(my_name)
-    my_var = input("Enter the number:")
-
-    if my_name == "Vikram" and my_var == 0:
-        print("Vikram Is Great !!")
-    elif(my_name == 'Shinde'):
-        print("you are also Great !!")
-    else:
-        print("hello world")
 
 helloWorld('Vikram')
