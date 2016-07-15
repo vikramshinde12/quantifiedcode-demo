62435174468de20cffc73543feffbc45551d239c
vikramshinde12 <vik.shinde@gmail.com>
2016-07-15 10:27:47 +0100

    Delete list.py

--- list.py
+++
@@ -1,5 +0,0 @@
-# Overwriting built-in 'list' by assigning values to a variable called 'list'
-car_list = [1, 2, 3]
-# Defining a list 'cars', will now raise an error
-cars = car_list()
-# Error: TypeError: 'list' object is not callable
