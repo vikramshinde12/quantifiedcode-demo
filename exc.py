bb9d046094e646844d124a86e0cf247f20ebe1f8
Vikram P. Shinde <vikramps@mastek.com>
2016-07-15 14:18:23 +0100

    exec

--- exc.py
+++ exc.py
@@ -1,4 +1,9 @@
 # ...
-title = request.form['title']
-s = "print {0}".format(title)
-exec(s)
+#title = request.form['title']
+#s = "print {0}".format(title)
+#exec(s)
+
+def print_hello():
+    print "Hello, World!"
+
+print_hello()
